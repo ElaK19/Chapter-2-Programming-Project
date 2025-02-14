a0537515335c409454621047841291a48aa4932c
Angela Paull <angelak1119@gmail.com>
2025-02-14 14:08:24 -0500

    Add project files.

---
+++ Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
@@ -0,0 +1,34 @@
+// Chapter 2 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Assuming the ocean’s level is currently rising at about 1.5 millimeters per year, write a program that displays: 
+//	The number of millimeteters higher than the current level that the ocean’s level will be in 5 years
+//	The number of millimeters higher than the current level that the ocean’s level will be in 7 years
+// The number of millimeters higher than the current level that the ocean’s level will be in 10 years
+int main()
+{
+	int number;
+	return 0
+
+		namespace :: OceanLevelCalculator
+	{
+		class program 
+			int main(string[] args)
+		{
+			//set the current rise rate per year//
+			double riseRate = 1.5;
+
+			//Calculate sea level rise after 5 years//
+			double levelAfter5Years = riseRate * 5;
+			("Sea level rise after 5 years: {0} millimeters", levelAfter5Years)
+
+			//Calculate sea level rise after 7 years//
+			double levelAfter7Years = riseRate * 7;
+			("Sea level rise after 7 years: {0} millimeters", levelAfter7Years)
+			
+			//Calculate seal level rise after 10 years//
+			double levelAfter10Years = riseRate * 10
+			("Sea level rise after 10 years: {0} millimeters", levelAfter10Years)
+
+		}
+	}
+
+}
